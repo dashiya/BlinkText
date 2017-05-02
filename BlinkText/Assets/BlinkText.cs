caa812246250dfaaa43b1b8ab49db9cc618cd64a
dashiya <hadashiya.duck@gmail.com>
2017-05-03 00:34:56 +0900

    Add BlinkText and MoveCube
    Success to change the color of text when called OnTriggerEnter and
    OnTrigerExit

---
+++ BlinkText/Assets/BlinkText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BlinkText : MonoBehaviour {
+
+    public Text text;
+    public MoveCube mc;
+
+	// Use this for initialization
+	void Start () {
+
+        mc = GameObject.Find("MoveCube").GetComponent<MoveCube>();
+
+       	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        BlinkJudge();
+
+	}
+
+    void BlinkJudge()
+    {
+        if ( mc.isTouch == true)
+        {
+            gameObject.GetComponent<Text>().color = new Color(255, 255, 255, 1); 
+        }
+        if( mc.isTouch == false)
+        {
+            gameObject.GetComponent<Text>().color = new Color(0,0,0,0);
+        }
+    }
+
+}
---
+++ BlinkText/Assets/MoveCube.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+
+
+    float X_Speed = 1;
+    float Z_Speed = 1;
+
+    public bool isTouch = false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float vertical = Input.GetAxis("Vertical");
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(0, 0, vertical * Z_Speed);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(0, 0, vertical * Z_Speed);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(horizontal * X_Speed, 0, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(horizontal * X_Speed, 0, 0);
+        }
+    }
+
+
+   void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.name == "Wall")
+        {
+            isTouch = true;
+            Debug.Log("Touch Wall");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.name == "Wall")
+        {
+            isTouch = false;
+        }
+    }
+
+}
+
+
